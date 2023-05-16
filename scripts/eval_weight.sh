abd6c986504d42520d839133c96a128c62697836
HeliosZhao <yuyangzhao@outlook.com>
2023-05-16 11:44:22 +0800

    update code

--- scripts/eval_weight.sh
+++
@@ -1,6 +0,0 @@
-
-## LR 3e-5, drop rate 0.2, 0.3, 0.5
-for NOISE in 1.0 0.8 0.5 0.3 0.1
-do
-    python eval_unclip_controlavideo_prior.py --config configs/ikun/sd2/eval-ikun-768-8-unclip-prior.yaml --options validation_data.interpolate_embed_weight=$NOISE
-done
--- scripts/git-clone-bash.sh
+++
@@ -1,9 +0,0 @@
-## in upper
-git clone https://<pat>@github.com/HeliosZhao/Control-A-Video.git
-
-## in Control-A-Video folder
-git pull origin main
-
-## force to overwrite
-git fetch --all
-git reset --hard origin/main
